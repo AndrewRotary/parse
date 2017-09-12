584cc22fad8a277fb6b4599c5b0cf7072f4d9b38
Andrei <andrewrotary@gmail.com>
2017-09-12 20:06:22 +0300

    remote selenium

--- src/main/java/com/currencyparser/config/CustomWebDriver.java
+++ src/main/java/com/currencyparser/config/CustomWebDriver.java
@@ -1,16 +1,10 @@
 package com.currencyparser.config;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.stereotype.Component;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * Use one custom webDriver
@@ -20,20 +14,15 @@ public class CustomWebDriver {
 
     public static WebDriver webDriver;
 
-    public CustomWebDriver() throws MalformedURLException {
-//
-//        System.setProperty("webdriver.chrome.driver","/etc/alternatives/google-chrome");
-//        ChromeDriverManager.getInstance().setup();
-//        ChromeOptions options = new ChromeOptions();
-////        options.setBinary("/usr/local/bin/chromedriver");
-//        options.addArguments("headless");
-//        options.addArguments("window-size=1900x3000");
-//        webDriver = new ChromeDriver(options);
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setBrowserName("firefox");
-        capabilities.setPlatform(Platform.LINUX);
-        webDriver = new RemoteWebDriver(new URL("http://188.225.84.108:4444/wd/hub/"), capabilities);
+    public CustomWebDriver() {
 
+//        System.setProperty("webdriver.chrome.driver","/etc/alternatives/google-chrome");
+        ChromeDriverManager.getInstance().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/opt/google/chrome/google-chrome");
+        options.addArguments("headless");
+        options.addArguments("window-size=1900x3000");
+        webDriver = new ChromeDriver(options);
     }
 
     public WebDriver getWebDriver() {
